1931e840e8f3bd7b033f97f5615be2c2c7848f18
Aaron Cimolini <aaron@cimolini.com>
2014-11-11 14:13:21 -0500

    Adds jquery cookie plugin and removes old get/set cookie functions.

--- dist/assets/wishlist.js
+++ dist/assets/wishlist.js
@@ -182,34 +182,6 @@ function loadTabProducts(el) {
         );
     }
 
-    function setCookie(name, value, expires, path, domain, secure) {
-
-        if (!name || !value) {
-            return false;
-        }
-
-        var str = name + '=' + encodeURIComponent(value);
-        
-        if (expires) str += '; expires=' + expires.toGMTString();
-        if (path)    str += '; path=' + path;
-        if (domain)  str += '; domain=' + domain;
-        if (secure)  str += '; secure';
-        
-        document.cookie = str;
-        return true;
-    }
-
-    function getCookie(name) {
-        var pattern = "(?:; )?" + name + "=([^;]*);?";
-        var regexp  = new RegExp(pattern);
-        
-        if (regexp.test(document.cookie)) {
-            return decodeURIComponent(RegExp["$1"]);
-        }
-        
-        return false;
-    }
-
     function createWishlist(title){
         closeMessage();
         $.getJSON(
